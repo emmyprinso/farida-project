4a2ad34449042f53c4a177e10fb88763cb17ea89
emmyprinso <emmyprinso@gmail.com>
2018-03-31 01:33:20 +0100

    trying to update

--- configuration.php
+++ configuration.php
@@ -4,18 +4,18 @@ $GLOBALS["serverUserName"] = "root";
 $GLOBALS["serverPassword"] = NULL;
 $GLOBALS["dbname"] = "farida";
 
-
+echo "call came in";
 
 //try to connect to the database
-$conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["serverUserName], $GLOBALS["serverPassword"], $GLOBALS["dbname"]);
+$conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["serverUserName"], $GLOBALS["serverPassword"], $GLOBALS["dbname"]);
 
 if(!$conn){		//connecting to the database failed
-	$conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["serverUserName"], GLOBALS["serverPassword"]);
+	$conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["serverUserName"], NULL);
 	if(!$conn){
 		die("Unable to connect to the server" . mysqli_connect_error($conn));
 	}else{		//creating the database on the server
 		$sql = "CREATE DATABASE " . $dbname;
-		if(mysqli_query($sql)){
+		if(mysqli_query($conn, $sql)){
 			echo "Successfully Created the Database " . $dbname;
 		}else{
 			echo "Unable to craete the database " . $dbname;
@@ -28,11 +28,11 @@ if(!$conn){		//connecting to the database failed
 //test to see if the Customers table is available
 $sql = "SELECT username, password FROM Customers";
 $result = mysqli_query($conn, $sql);
-if(!(mysqli_num_rows($results) > 0)){		//where the table does not exist
+if(!(mysqli_num_rows($result) > 0)){		//where the table does not exist
 	//create the table with customers informations 
 	$sql = "CREATE TABLE Customers(
 			id INT(6) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-			firstname VARCHAR(20) NOT NULL,
+			firstname VARCHAR(30) NOT NULL,
 			lastname VARCHAR(20) NOT NULL,
 			username VARCHAR(20) NOT NULL,
 			password VARCHAR(20) NOT NULL,
@@ -53,7 +53,7 @@ if(!(mysqli_num_rows($results) > 0)){		//where the table does not exist
 //if there is none start a session
 
 if(empty($_SESSION["username"])){		//section not started
-	session_start();		//start session
+
 }
 
 
