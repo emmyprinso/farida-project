4186fd7cfd15a66994e1a67a34312ead70eb609b
emmyprinso <emmyprinso@gmail.com>
2018-03-31 03:54:06 +0100

    cart content update

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 include "configuration.php";
-ini_set('display_errors', false);				//REMEMBER TO RE ACTIVATE ERROR WHEN NECESSARY
+
+
+//ini_set('display_errors', false);				//REMEMBER TO RE ACTIVATE ERROR WHEN NECESSARY
+
+
 $username = NULL;
 $password = NULL;
 echo "after include";
@@ -67,8 +71,8 @@ catch(Exception $e){
 	<div id="regButtonDiv" style="height:37px;margin:10px;float:left;">
 
 <?php
-	if (username != NULL || password != NULL){
-		echo '<a href="login.php"><button	class="searchCollection">Logout</button></a>';
+	if ($username != NULL || $password != NULL){
+		echo '<a href="logout.php"><button	class="searchCollection">Logout</button></a>';
 	}else{
 		echo '<a href="login.php"><button	class="searchCollection">Login</button></a>';
 	}
@@ -79,7 +83,7 @@ catch(Exception $e){
 	<a href="registration.php"><button	class="searchCollection">Register</button></a>
 	
 	<?php
-	if (username != NULL || password != NULL){
+	if ($username != NULL || $password != NULL){
 		echo "<br>".$username;
 	}
 	?>
@@ -88,7 +92,43 @@ catch(Exception $e){
 
 	
 	<div id="slogan">
-	<span id="cartQuantity" style="color:red;">0</span>
+	
+	
+
+
+	<?php
+	$cart = 0;
+	if ($username != NULL || $password != NULL){
+	$conn = $conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["serverUserName"], $GLOBALS["serverPassword"], $GLOBALS["dbname"]);
+	if(!$conn){
+		die("Connection Failed ".mysqli_connect_error);
+	}else{
+		$sql = "SELECT cart FROM $username";
+		$result = mysqli_query($conn, $sql);
+		//$cart = count($result);
+		if(mysqli_num_rows($result) > 0){
+			while($row = mysqli_fetch_assoc($result)){
+					++$cart;
+					}
+			}
+	}
+	mysqli_close($conn);
+		
+	}
+	
+	echo '<span id="cartQuantity" style="color:red;">' . $cart . '</span>';
+	?>	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 612.322 612.322" style="enable-background:new 0 0 612.322 612.322;" xml:space="preserve" width="25px" height="25px">
 			<g><g><g>
 				<path d="M187.57,359.458h360.895l63.856-227.934h-93.973l-33.74-104.22L343.9,72.916l19.244,58.608h-6.623l-34.74-104.22     L181.072,72.916l19.244,58.608h-75.228l-15.62-62.482c-7.873-31.241-35.115-53.359-66.481-53.359H0V41.8h42.987     c19.494,0,36.489,12.996,41.738,32.616l69.105,270.421c-14.246,10.247-23.618,26.992-23.618,46.112     c0,31.241,26.117,57.358,57.358,57.358h351.773V422.19H187.57c-16.995,0-31.241-14.371-31.241-31.241     C156.33,373.829,170.7,359.458,187.57,359.458L187.57,359.458z M467.739,59.795l23.493,70.355H389.637l-14.371-40.363     L467.739,59.795z M304.911,59.795l23.493,70.355H226.809l-14.371-40.363L304.911,59.795z M578.457,156.267l-49.486,177.199     H177.198l-45.612-177.198L578.457,156.267L578.457,156.267z" fill="#dc4e04"/>
